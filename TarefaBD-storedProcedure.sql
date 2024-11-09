879bcfcd63a116cb529fb4e0ed295c806312b6c8
Thiago Hens Suchi <166747903+ThiagoSuchi@users.noreply.github.com>
2024-11-09 17:45:00 -0400

    Add files via upload

---
+++ TarefaBD-storedProcedure.sql
@@ -0,0 +1,28 @@
+-- Exercício 01 de BD:
+DELIMITER //
+create procedure atualizaPrecoLivro (in num_Livro varchar(50), in novo_Preco decimal(10,2))
+begin
+
+	declare livro varchar(50);
+    
+    select count(*) into livro
+    from livros
+    where numero = num_Livro;
+    
+    if livro = 0 then
+		select 'Livro não existe' as mensagem;
+	else 
+		update livros
+        set preco = novo_Preco
+        where numero = num_Livro;
+		
+		select 'Atualizado com sucesso' as mensagem;
+    end if;
+
+end//
+DELIMITER ;
+drop procedure atualizaPrecoLivro;
+call atualizaPrecoLivro(10277843, 20.00);
+select*from livros;
+
+-- Exercício 02 de BD:
